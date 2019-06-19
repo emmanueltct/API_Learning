390ddd334fe453a5c92c0d30d40b5b5d9a5971fa
Emmy <emmanuelmunezero@gmail.com>
2019-06-19 19:07:49 +0200

    [Finishes

---
+++ src/controller/car/singlecar.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import pool from '../../config/config.js'
+
+const singleCar=(req,res)=>{
+const car_id=req.params.id;
+
+const selectQuery = 'SELECT * FROM cars WHERE id=$1';
+pool.query(selectQuery,[car_id], (error,answer)=>{
+
+    if(answer.rows.length>0){
+        return res.status(200).json({
+            status:200,
+            data:answer.rows[0]
+        })
+    }else{
+        return res.status(404).json({
+            status:404,
+            message:`no car found on this id ${car_id} `
+        }) 
+    }
+
+})
+  
+};
+
+    export default singleCar;
